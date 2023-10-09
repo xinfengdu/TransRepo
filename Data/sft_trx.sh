b674d813d49f8585a54d31fedbc40e391faee930
DXF <51333202+xinfengdu@users.noreply.github.com>
2023-10-09 17:28:53 +0800

    Add files via upload

---
+++ Data/sft_trx.sh
@@ -0,0 +1,25 @@
+hostfile=""
+deepspeed --hostfile=$hostfile fine-tune.py  \
+    --report_to "none" \
+    --data_path "/data1/duxinfeng/YxProjects/yxLLM/Baichuan/Baichuan2/fine-tune/data/tune.json" \
+    --model_name_or_path "/data1/duxinfeng/YxProjects/yxLLM/Baichuan/Baichuan2-7B-Chat" \
+    --output_dir "output_100915_10epoch" \
+    --model_max_length 512 \
+    --num_train_epochs 10 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --save_strategy epoch \
+    --learning_rate 2e-5 \
+    --lr_scheduler_type constant \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.98 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --weight_decay 1e-4 \
+    --warmup_ratio 0.0 \
+    --logging_steps 100 \
+    --gradient_checkpointing True \
+    --deepspeed ds_config.json \
+    --bf16 True \
+    --tf32 True \
+    --use_lora True
